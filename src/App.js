dabc070bfbefc3d813b9f128f8b3671c9b2f2903
Orkid <mrs.emadi@yahoo.com>
2021-10-29 09:55:20 +0330

    ADD:routes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom' 
 import Login from './components/Login';
+import Header from './components/Header';
+import Home from './components/Home';
 
 
 function App() {
   return (
     <div className="App">
      <Router>
+      <Header />
        <Switch>
          <Route exact path='/' component={Login} />
+         <Route path='/home' component={Home} />
        </Switch>
      </Router>
     </div>
